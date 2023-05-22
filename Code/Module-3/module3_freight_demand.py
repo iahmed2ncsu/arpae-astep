55846f2f3c652922fc9c229e448dd51d7dc7cbd5
iahmed2 <iahmed2@ncsu.edu>
2023-05-22 13:32:17 -0400

    changed csv output with temp function in Code/Module-3

--- Code/Module-3/module3_freight_demand.py
+++ Code/Module-3/module3_freight_demand.py
@@ -14,6 +14,7 @@ import numpy as np
 import os
 import time
 import streamlit as st
+import tempfile
 
 ###################
 ### Paths and Files
@@ -593,10 +594,8 @@ def create_total_flow(years, freight_demand_scenario, econ_scn, fossilfuel_reduc
                 total_flow = total_flow + flow
     
     net_flows = generate_net_flows(total_flow)
-    with open ("%s/odflows.txt" %(module3_output_path),"w") as output:
-        output.write(net_flows)
-    
-    #net_flows.to_csv("%s/odflows.txt" %(module3_output_path), sep="\t", header=None, index=False)
+    with tempfile.NamedTemporaryFile(mode="w+",delete=False,dir= './Data/Output/Module-3',suffix='.txt'):
+        net_flows.to_csv("odflows.txt", sep="\t", header=None, index=False)
     #net_flows.to_csv("%s/odflows.txt" %(path_trafassign), sep="\t", header=None, index=False)
     #end = time.time()
     return net_flows
